c4ca2058379134fd90cc6fc2b025ce6cf6baed10
PoLLoB19 <pollobatwork19@gmail.com>
2024-03-21 23:04:54 +0600

    gsdg

---
+++ script.js
@@ -0,0 +1,13 @@
+let a=0;
+
+function add(){
+
+document.querySelector('.hold').innerHTML =a=a+1;
+}
+
+
+function dark(){
+    document.body.style.backgroundColor = "#dc143c";
+}
+
+document.getElementById('mo').addEventListener("click",dark)
