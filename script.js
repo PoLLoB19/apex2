36d4be6be3a0186e630f7443460e2e1a9f65b6a6
PoLLoB19 <pollobatwork19@gmail.com>
2024-03-25 14:32:18 +0600

    wda

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-let a=0;
+let a = 0;
 
-function add(){
-
-document.querySelector('.hold').innerHTML =a=a+1;
+function add() {
+  document.querySelector(".hold").innerHTML = a = a + 1;
 }
 
+let items = [
+  shoe1 = {
+    Name: "Sprint Men's Sports Shoe",
+    price: 30,
+    type: "Shoe",
+  },
+  shoe2 = {
+    Name: "Sprint Men's Sports Shoe",
+    price: 36,
+    type: "Shoe",
+  },
+  shoe3 = {
+    Name: "Sprint Men's Sports Shoe",
+    price: 28,
+    type: "Shoe",
+  },
+  shoe4 = {
+    Name: "Sprint Men's Sports Shoe",
+    price: 32,
+    type: "Shoe",
+  },
+];
 
-function dark(){
-    document.body.style.backgroundColor = "#dc143c";
+
+function af(){
+    document.querySelector('cart-items').innerHTML=items[0]
 }
-
-document.getElementById('mo').addEventListener("click",dark)
