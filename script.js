03532a0fbf340ae6fee056a967514ee9517e5184
PoLLoB19 <pollobatwork19@gmail.com>
2024-03-26 00:28:39 +0600

    done

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function af() {
   let to=  document.createElement('div');
    to.innerHTML=`<div style="padding-bottom:15px; padding-left:15px"> <div>Name- ${items[0].Name}</div>  <div>Price- ${items[0].price}$</div> <div>Type- ${items[0].type}</div> </div> `;
  
-   document.querySelector('.cart-items').append(to)
+   document.querySelector('.ekhane').append(to)
 
  
 }
@@ -42,23 +42,56 @@ function af() {
 function afa() {
   let to=  document.createElement('div');
    to.innerHTML=`<div style="padding-bottom:15px; padding-left:15px"> <div>Name- ${items[1].Name}</div>  <div>Price- ${items[1].price}$</div> <div>Type- ${items[1].type}</div></div> `;
-   document.querySelector('.cart-items').append(to)
+   document.querySelector('.ekhane').append(to)
  
 }
 
 function afb() {
   let to=  document.createElement('div');
    to.innerHTML=`<div style="padding-bottom:15px; padding-left:15px">  <div>Name- ${items[2].Name}</div>  <div>Price- ${items[2].price}$</div> <div>Type- ${items[2].type}</div></div> `;
-   document.querySelector('.cart-items').append(to)
+   document.querySelector('.ekhane').append(to)
  
 }
 
 function afc() {
   let to=  document.createElement('div');
    to.innerHTML=`<div style="padding-bottom:15px; padding-left:15px"> <div>Name- ${items[3].Name}</div>  <div>Price- ${items[3].price}$</div> <div>Type- ${items[3].type}</div></div> `;
-   document.querySelector('.cart-items').append(to)
+   document.querySelector('.ekhane').append(to)
  
 }
+
+
+
+  let total =0;
+  function jha(){
+     total= total + items[0].price;
+     
+     document.querySelector('.sub').innerHTML=`Subtotal: $${total}`;
+  }
+  function jh2(){
+    total= total + items[1].price;
+    
+    document.querySelector('.sub').innerHTML=`Subtotal: $${total}`;
+ }
+ function jh3(){
+  total= total + items[2].price;
+  
+  document.querySelector('.sub').innerHTML=`Subtotal: $${total}`;
+}
+function jh4(){
+  total= total + items[3].price;
+  
+  document.querySelector('.sub').innerHTML=`Subtotal: $${total}`;
+}
+ 
+
+ 
+
+
+
+
+
+
 
 function ber(){
   document.querySelector('.cart-items').classList.add("cca")
