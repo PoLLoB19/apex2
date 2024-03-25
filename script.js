708dc37196f83880eea44a13b5757d56ee17aa8d
PoLLoB19 <pollobatwork19@gmail.com>
2024-03-25 16:44:27 +0600

    click

--- script.js
+++ script.js
@@ -4,23 +4,26 @@ function add() {
   document.querySelector(".hold").innerHTML = a = a + 1;
 }
 
+
+
 let items = [
-  shoe1 = {
+  {
     Name: "Sprint Men's Sports Shoe",
     price: 30,
     type: "Shoe",
+    pic:"pic/shoes/sh/2e8cd738-d0d3-4e1f-a6e4-d4ca559a9b37"
   },
-  shoe2 = {
+ {
     Name: "Sprint Men's Sports Shoe",
     price: 36,
     type: "Shoe",
   },
-  shoe3 = {
+  {
     Name: "Sprint Men's Sports Shoe",
     price: 28,
     type: "Shoe",
   },
-  shoe4 = {
+   {
     Name: "Sprint Men's Sports Shoe",
     price: 32,
     type: "Shoe",
@@ -28,6 +31,43 @@ let items = [
 ];
 
 
-function af(){
-    document.querySelector('cart-items').innerHTML=items[0]
+
+function af() {
+  let to=  document.createElement('div');
+   to.innerHTML=`<div style="padding-bottom:15px; padding-left:15px"> <div>Name- ${items[0].Name}</div>  <div>Price- ${items[0].price}$</div> <div>Type- ${items[0].type}</div> </div> `;
+ 
+   document.querySelector('.cart-items').append(to)
+
+ 
+}
+
+function afa() {
+  let to=  document.createElement('div');
+   to.innerHTML=`<div style="padding-bottom:15px; padding-left:15px"> <div>Name- ${items[1].Name}</div>  <div>Price- ${items[1].price}$</div> <div>Type- ${items[1].type}</div></div> `;
+   document.querySelector('.cart-items').append(to)
+ 
+}
+
+function afb() {
+  let to=  document.createElement('div');
+   to.innerHTML=`<div style="padding-bottom:15px; padding-left:15px">  <div>Name- ${items[2].Name}</div>  <div>Price- ${items[2].price}$</div> <div>Type- ${items[2].type}</div></div> `;
+   document.querySelector('.cart-items').append(to)
+ 
+}
+
+function afc() {
+  let to=  document.createElement('div');
+   to.innerHTML=`<div style="padding-bottom:15px; padding-left:15px"> <div>Name- ${items[3].Name}</div>  <div>Price- ${items[3].price}$</div> <div>Type- ${items[3].type}</div></div> `;
+   document.querySelector('.cart-items').append(to)
+ 
+}
+
+function ber(){
+  document.querySelector('.cart-items').classList.add("cca")
+  document.querySelector('.ca').classList.add("cate")
+}
+
+function be(){
+  document.querySelector('.cart-items').classList.remove("cca")
+  document.querySelector('.ca').remove("cate")
 }
